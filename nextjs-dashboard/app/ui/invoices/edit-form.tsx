fa9071fbbb75587d7484b3a73c3951b3be5bc094
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2024-04-28 19:16:16 +0900

    feat: 認証機能を実装+フォーマッター実行 (#14)

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -37,7 +37,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
-              aria-describedby='customer-error'
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -75,7 +75,7 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                aria-describedby='customer-error'
+                aria-describedby="customer-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -105,7 +105,7 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                  aria-describedby='customer-error'
+                  aria-describedby="customer-error"
                 />
                 <label
                   htmlFor="pending"
@@ -141,11 +141,9 @@ export default function EditInvoiceForm({
               ))}
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.message &&
-              <p className="mt-2 text-sm text-red-500">
-                {state.message}
-              </p>
-            }
+            {state.message && (
+              <p className="mt-2 text-sm text-red-500">{state.message}</p>
+            )}
           </div>
         </fieldset>
       </div>
